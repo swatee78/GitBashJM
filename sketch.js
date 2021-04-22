abe1cc53aef5bd375924319c33c72dc112e89f53
swatee78 <swatchat1@gmail.com>
2021-04-22 16:33:46 +0530

    console.log message

--- sketch.js
+++ sketch.js
@@ -34,15 +34,15 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    console.log("GITBASH");
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+  //  console.log(box2.body.position.x);
+  //  console.log(box2.body.position.y);
+  //  console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
